571f51a910c91c682073ac3f3515b4d240bb0663
Laurent Bodelet <laurent.bodelet@gmail.com>
2025-12-03 11:40:29 +0100

    ajout findUnreadByUser

--- src/Repositories/NotificationRepository.php
+++ src/Repositories/NotificationRepository.php
@@ -6,5 +6,52 @@ use Doctrine\ODM\MongoDB\Repository\DocumentRepository;
 
 class NotificationRepository extends DocumentRepository
 {
+    /***
+     * @param string $userId
+     * @param string $serviceName
+     * @param int $limit
+     * @return array
+     * - Retourne les notifications non lues d'un utilisateur
+     * - Triées par date de création décroissante
+     * - Limitées à `$limit` résultats
+     * - Utilise une projection pour ne récupérer que les champs nécessaires
+     */
+    public function findUnreadByUser(string $userId, string $serviceName, int $limit = 20): array
+    {
+        return $this->createQueryBuilder()
+            ->field('userId')->equals($userId)// d'un utilisateur
+            ->field('readAt')->equals(null)// non lues
+            ->field('serviceName')->equals($serviceName)
+            ->sort('createdAt', 'DESC')//- Triées par date de création décroissante
+            ->sort('id', 'DESC')//- Triées par id si il y a des problèmes de microseconde
+            ->limit($limit) //Limitées à `$limit` résultats
+            ->getQuery()
+            ->execute()
+            ->toArray();
+    }
+
+    /**
+     * @param string $status
+     * @param string $serviceName
+     * @param \DateTime $startDate
+     * @param \DateTime $endDate
+     * @return int
+     * - Compte les notifications par statut et service dans une période
+     * - Utilise une agrégation MongoDB optimisée
+     */
+    public function countByStatusAndService(string $status, string $serviceName, \DateTime $startDate, \DateTime $endDate):int
+    {
+
+    }
+
+    public function getStatisticsByService(string $serviceName, \DateTime $startDate, \DateTime $endDate)
+    {
+
+    }
+
+    public function findFailedNotificationsOlderThan(int $hours)
+    {
+
+    }
 
 }
--- tests/Repositories/NotificationRepositoryTest.php
+++ tests/Repositories/NotificationRepositoryTest.php
@@ -3,6 +3,8 @@
 namespace App\Tests\Repositories;
 
 use App\Document\Notification;
+use App\Enum\NotificationServiceName;
+use App\Enum\NotificationTypes;
 use App\Repositories\NotificationRepository;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -17,6 +19,7 @@ class NotificationRepositoryTest extends KernelTestCase
         self::bootKernel();
 
         // Utiliser le document manager de test
+        /** @var DocumentManager dm */
         $this->dm = self::getContainer()->get('doctrine_mongodb.odm.test_document_manager');
         $this->repository = $this->dm->getRepository(Notification::class);
 
@@ -33,7 +36,62 @@ class NotificationRepositoryTest extends KernelTestCase
     }
 
     //test pour findUnreadByUser
-    public function testFindUnreadByUser(): void{
-        //création
+    public function testFindUnreadByUser(): void
+    {
+        $userId = "user-1";
+        //création d'une notification non lue
+        $unreadNotification = new Notification($userId, NotificationTypes::INFO->value, "Non lue 1", "Le message non lu", NotificationServiceName::DIABETES->value, []);
+        $unreadNotification2 = new Notification($userId, NotificationTypes::ALERT->value, "Non lue 2", "Le message non lu 2", NotificationServiceName::DIABETES->value, []);
+        $readNotification = new Notification($userId, NotificationTypes::INFO->value, "lue 1", "Le message lu", NotificationServiceName::DIABETES->value, []);
+        $readNotification->setReadAt(new \DateTimeImmutable());
+
+        $this->dm->persist($readNotification);
+        $this->dm->persist($unreadNotification);
+        $this->dm->persist($unreadNotification2);
+        $this->dm->flush();
+
+        //passage au tests
+
+        $unreadNotifications = $this->repository->findUnreadByUser($userId,NotificationServiceName::DIABETES->value);
+        $this->assertCount(2, $unreadNotifications);
+        $this->assertNull($unreadNotifications[0]->getReadAt());
+        $this->assertNull($unreadNotifications[1]->getReadAt());
+    }
+
+    public function testLimitDefaultFindUnreadByUser(): void{
+        $userId = "user-2";
+        for($i=0;$i<21;$i++){
+            $unreadNotification = new Notification($userId, NotificationTypes::INFO->value, "Non lue $i", "Le message non lu", NotificationServiceName::DIABETES->value, []);
+            $this->dm->persist($unreadNotification);
+        }
+
+        $this->dm->flush();
+        $unreadNotifications = $this->repository->findUnreadByUser($userId,NotificationServiceName::DIABETES->value);
+        $this->assertCount(20, $unreadNotifications);
+
+        $lastDateCreate = null;
+        //vérification de l'ordre
+        foreach ($unreadNotifications as $unreadNotification) {
+            if($lastDateCreate === null){
+                $lastDateCreate = $unreadNotification->getCreatedAt();
+            }else{
+                $this->assertTrue($unreadNotification->getCreatedAt()<$lastDateCreate);
+                $lastDateCreate = $unreadNotification->getCreatedAt();
+            }
+        }
+    }
+    public function testLimitFindUnreadByUser(): void{
+        $userId = "user-3";
+        for($i=0;$i<10;$i++){
+            $unreadNotification = new Notification($userId, NotificationTypes::INFO->value, "Non lue $i", "Le message non lu", NotificationServiceName::DIABETES->value, []);
+            $this->dm->persist($unreadNotification);
+        }
+
+        $this->dm->flush();
+
+        $unreadNotifications = $this->repository->findUnreadByUser($userId,NotificationServiceName::DIABETES->value,5);
+        $this->assertCount(5, $unreadNotifications);
+
+
     }
 }
